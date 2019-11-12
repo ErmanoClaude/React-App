04f08db79e6f5bb48a344e997c12d1b00f0e73d9
Ermano Claude <ermano.claude@cix.csi.cuny.edu>
2019-11-12 01:41:28 -0500

    implimenting SCSS

--- src/pages/homePageComponent.js
+++ src/pages/homePageComponent.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import classNames from 'classnames'
 class Home extends React.Component {
     exampleMethod(){
         console.log("JS is running")
--- webpack.client.js
+++ webpack.client.js
@@ -2,7 +2,7 @@ const path = require('path');
 const webpackNodeExternals = require('webpack-node-externals')
 module.exports = {
     target: 'node',
-    entry: './src/client.js',
+    entry: ['./src/client.js', './src/assets/scss/styles.scss'],
     output: {
         filename: 'client_bundle.js',
         path: path.resolve(__dirname, 'build/public'),
@@ -23,7 +23,29 @@ module.exports = {
                         }]
                     ]
                 }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].min.css',
+                            outputPath: 'assets/css'
+                        }
+                    },
+                    {
+                        loader: 'extract-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ]
             }
+
         ]
     }
 }
