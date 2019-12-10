03f25ed5eaddc485b9fcd2d86dd6db85b91f40d1
ErmanoClaude <ermano.claude@cix.csi.cuny.edu>
2019-12-09 22:21:56 -0500

    forcing https

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import bodyParser from 'body-parser'
 import renderer from './src/helpers/renderer'
 import helmet from 'helmet'
+import express_enforces_ssl from 'express-enforces-ssl'
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,6 +12,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('build/public'))// this makes it publically avaliable
 app.use(helmet());
+
+app.enable('trust proxy');
+app.use(express_enforces_ssl());
 
 app.get('*',(req,res)=>{
     const context = {};
