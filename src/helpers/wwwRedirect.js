449cc1a91a7e923902e1cf658e63754f211544a3
ErmanoClaude <ermano.claude@cix.csi.cuny.edu>
2019-12-09 22:37:53 -0500

    non www function not working

--- src/helpers/wwwRedirect.js
+++
@@ -1,7 +0,0 @@
-export default (req, res, next) => {
-    if (req.headers.host.slice(0, 4) === 'www.') {
-        var newHost = req.headers.host.slice(4);
-        return res.redirect(301, req.protocol + '://' + newHost + req.originalUrl);
-    }
-    next();
-};
