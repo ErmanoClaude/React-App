e489a959e67b5be9bb1f16650735afbba34aca37
ErmanoClaude <ermano.claude@cix.csi.cuny.edu>
2019-12-09 23:02:06 -0500

    Working on non www to www

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import 'babel-polyfill';
 import express from 'express';
-import bodyParser from 'body-parser'
-import renderer from './src/helpers/renderer'
-import helmet from 'helmet'
-import express_enforces_ssl from 'express-enforces-ssl'
+import bodyParser from 'body-parser';
+import renderer from './src/helpers/renderer';
+import helmet from 'helmet';
+import express_enforces_ssl from 'express-enforces-ssl';
+import webConfig from './webConfig.json'
+import force from "express-force-domain";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -15,6 +17,9 @@ app.use(helmet());
 
 app.enable('trust proxy');
 app.use(express_enforces_ssl());
+
+//When testing comment this out
+app.use(force(webConfig.customUrl))
 
 app.get('*',(req,res)=>{
     const context = {};
